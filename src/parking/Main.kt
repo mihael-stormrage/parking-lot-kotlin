6ab8168afc78a5524dd98f6c8f29c9b50b937bf6
Mihael Stormrage <mihan.msn@hotmail.com>
2020-07-03 04:36:16 +0700

    Stage #3: Expand and park

--- src/parking/Main.kt
+++ src/parking/Main.kt
@@ -5,14 +5,30 @@ import java.util.*
 val scanner = Scanner(System.`in`)
 
 fun main() {
-    when (scanner.next()) {
-        "park" -> {
-            scanner.next()
-            println("${scanner.next()} car parked in spot 2.")
+    val parking = BooleanArray(20)
+
+    do {
+        val command = scanner.next()
+
+        when (command) {
+            "park" -> {
+                scanner.next()
+
+                val firstFree = parking.indexOfFirst { !it }
+                val freeSpot = if (firstFree == -1) 0 else firstFree
+
+                println(if (parking.all { it }) "Sorry, the parking lot is full." else {
+                    parking[freeSpot] = true
+                    "${scanner.next()} car parked in spot ${freeSpot + 1}."
+                })
+            }
+            "leave" -> {
+                val spot = scanner.nextInt()
+                println(if (parking[spot - 1]) {
+                    parking[spot - 1] = false
+                    "Spot $spot is free."
+                } else "There is no car in spot $spot.")
+            }
         }
-        "leave" -> {
-            val spot = scanner.nextInt()
-            println(if (spot == 1) "Spot 1 is free." else "There is no car in spot $spot.")
-        }
-    }
+    } while (command != "exit")
 }
